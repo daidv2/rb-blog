216c43cbed40197f9240e36bf1c9d3b9795f5e3f
DaiDV <daidv@rikkeisoft.com>
2018-07-24 14:54:25 +0700

    CRUD users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def maketitle(title = '')
+    title + ' | Hoc ruby';
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   validates :email,
-    length: {in: 2...10, too_long: 'Ban da nhap email qua dai'},
-    presence: true
+            length: {in: 2..255, too_long: 'Ban da nhap email qua dai'},
+            presence: true,
+            uniqueness: {case_sensitive: false}
+
+  validates :name, presence: true, length: {in: 4..255}
 
   validate :check_name_valid
 
+  # uncomment in Gemfile gem 'bcrypt', '~> 3.1.7'
+  has_secure_password
+
   def check_name_valid
-    if(name.present? && name == email)
+    if (name.present? && name == email)
       errors.add(:name, 'Ten khong duoc trung voi email')
     end
   end
